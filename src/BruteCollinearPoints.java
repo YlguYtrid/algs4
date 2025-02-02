9a865380d5d912f478dcad1ff37a82dd57280720
Ylgu Ytrid <37318504+YlguYtrid@users.noreply.github.com>
2025-02-02 17:43:31 +0800

    Algorithms, Part I, Module 6, Collinear Points :: Retrive Code

---
+++ src/BruteCollinearPoints.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    private final LineSegment[] segments;
+
+    public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
+        if (points == null)
+            throw new IllegalArgumentException("points is null");
+        for (Point p : points)
+            if (p == null)
+                throw new IllegalArgumentException("points contains null");
+        Point[] pointsSorted = points.clone();
+        Arrays.sort(pointsSorted);
+        for (int i = 0; i < pointsSorted.length - 1; i++)
+            if (pointsSorted[i].equals(pointsSorted[i+1]))
+                throw new IllegalArgumentException("repeated points");
+        if (pointsSorted.length < 4) {
+            segments = new LineSegment[0];
+            return;
+        }
+        ArrayList<LineSegment> list = new ArrayList<>();
+        for (int i = 0; i < pointsSorted.length - 3; i++) {
+            for (int j = i+1; j < pointsSorted.length - 2; j++) {
+                for (int k = j+1; k < pointsSorted.length - 1; k++) {
+                    for (int l = k+1; l < pointsSorted.length; l++) {
+                        Point p = pointsSorted[i];
+                        Point q = pointsSorted[j];
+                        Point r = pointsSorted[k];
+                        Point s = pointsSorted[l];
+                        if (p.slopeTo(q) == p.slopeTo(r) && p.slopeTo(q) == p.slopeTo(s))
+                            list.add(new LineSegment(p, s));
+                    }
+                }
+            }
+        }
+        segments = new LineSegment[list.size()];
+        list.toArray(segments);
+    }
+
+    public int numberOfSegments() { // the number of line segments
+        return segments.length;
+    }
+
+    public LineSegment[] segments() { // the line segments
+        return segments.clone();
+    }
+}
---
+++ src/FastCollinearPoints.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FastCollinearPoints {
+    private final LineSegment[] segments;
+
+    public FastCollinearPoints(Point[] points) { // finds all line segments containing 4 or more points
+        if (points == null)
+            throw new IllegalArgumentException("points is null");
+        for (Point p : points)
+            if (p == null)
+                throw new IllegalArgumentException("points contains null");
+        Point[] pointsSorted = points.clone();
+        Arrays.sort(pointsSorted);
+        for (int i = 0; i < pointsSorted.length - 1; i++)
+            if (pointsSorted[i].equals(pointsSorted[i+1]))
+                throw new IllegalArgumentException("repeated points");
+        if (pointsSorted.length < 4) {
+            segments = new LineSegment[0];
+            return;
+        }
+        ArrayList<LineSegment> list = new ArrayList<>();
+        for (Point p : pointsSorted) {
+            Point[] pointsBySlope = pointsSorted.clone();
+            Arrays.sort(pointsBySlope, p.slopeOrder());
+            int i = 1;
+            while (i < pointsBySlope.length - 2) {
+                int j = i + 1;
+                while (j < pointsBySlope.length && pointsBySlope[i].slopeTo(p) == pointsBySlope[j].slopeTo(p))
+                    j++;
+                if (j - i >= 3 && p.compareTo(pointsBySlope[i])<0)
+                    list.add(new LineSegment(p, pointsBySlope[j-1]));
+                i = j;
+            }
+        }
+        segments = new LineSegment[list.size()];
+        list.toArray(segments);
+    }
+
+    public int numberOfSegments() { // the number of line segments
+        return segments.length;
+    }
+
+    public LineSegment[] segments() { // the line segments
+        return segments.clone();
+    }
+}
